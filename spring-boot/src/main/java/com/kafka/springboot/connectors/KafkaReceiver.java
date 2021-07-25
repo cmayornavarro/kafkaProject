a30d84b9dc39c9ffda4a96d68eab5fbcf7bd5e33
cmayornavarro <cmayornavarro@gmail.com>
2021-07-25 18:32:18 +0200

    new message received from kafka

--- spring-boot/src/main/java/com/kafka/springboot/connectors/KafkaReceiver.java
+++ spring-boot/src/main/java/com/kafka/springboot/connectors/KafkaReceiver.java
@@ -1,5 +1,7 @@
 package com.kafka.springboot.connectors;
+import org.slf4j.Logger;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.logging.log4j2.Log4J2LoggingSystem;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -10,7 +12,8 @@ import org.springframework.stereotype.Component;
 
         @KafkaListener(topics = "someTopic")
         public void processMessage(String content) {
-           System.out.println(content);
+           System.out.println("Message receive from kafka: "+ content);
+
         }
 
     }
